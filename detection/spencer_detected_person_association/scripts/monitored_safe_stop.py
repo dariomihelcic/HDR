7ba525596f594bae7c610335bf7569002987c1f0
Dario Mihelcic <dario.mihelcic@gmail.com>
2020-06-21 16:09:33 +0200

    Clean, Ferit ds, changes

--- detection/spencer_detected_person_association/scripts/monitored_safe_stop.py
+++ detection/spencer_detected_person_association/scripts/monitored_safe_stop.py
@@ -7,31 +7,46 @@ from geometry_msgs.msg import PoseStamped
 from std_msgs.msg import Int16
 
 #Will be published on the parameters server/topic in the future
-robot= {'x': 2, 'y': -2, 'reach': 1.8} 
+#robot= {'x': 2, 'y': -2, 'reach': 1.8} 
+#IR focal length of Kinect 1 camera ~[6.1, 6.5] mm
+robot= {'x': 0, 'y': 0, 'reach': 0}
+time_delay = 3
+stop_sign = 0
 
 def newDetectedPersonsAvailable(detectedPersons):
     try:
-        tfListener.waitForTransform(detectedPersons.header.frame_id, "odom", detectedPersons.header.stamp, rospy.Duration(0.05))
+        tfListener.waitForTransform(detectedPersons.header.frame_id, "odom",
+				 detectedPersons.header.stamp, rospy.Duration(0.05))
     except tf.Exception:
         return
 
+    global stop_sign
+    global last_detection_time
     for detectedPerson in detectedPersons.detections:
         poseStamped = PoseStamped()
         poseStamped.pose = detectedPerson.pose.pose
         poseStamped.header = detectedPersons.header
 
         transformedPoseStamped = tfListener.transformPose("odom", poseStamped)
-
         pos = transformedPoseStamped.pose.position
-        stop_sign = 1 if ((robot['x']-pos.x)**2 + (robot['y']-pos.y)**2)**(1.0/2) <= (robot['reach'] + pos.z) else 0
         
-        stopSignPublisher.publish(stop_sign)
+        distance = ((robot['x']-pos.x)**2 + (robot['y']-pos.y)**2)**(1.0/2)
+        #<= (robot[rech] + pos.z)
+        if distance <= (2):
+            stop_sign = 1
+            last_detection_time = rospy.get_rostime()
+     
+    current_time = rospy.get_rostime()
+    if stop_sign and (current_time.secs - last_detection_time.secs > time_delay):
+        stop_sign = 0
+        
+    stopSignPublisher.publish(stop_sign)
 
     global firstMessageOK
     if not firstMessageOK:
         firstMessageOK = True
         rospy.loginfo("First detections have been received, transformed!")
-        rospy.loginfo("{}\t{}\t{}\t{}\n{}\n" .format(pos.x, pos.y, pos.z, detectedPersons.header.stamp.to_sec(), stop_sign))
+        #rospy.loginfo("{}\t{}\t{}\t{}\n{}\n" .format(pos.x, pos.y, pos.z, detectedPersons.header.stamp.to_sec(), stop_sign))
 
 
 if __name__ == '__main__':
